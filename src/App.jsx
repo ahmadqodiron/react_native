02a152c9cf1cb48fbd4355a685e7b02d6e61a764
unknown <riyanmorvin25@gmail.com>
2024-05-05 11:48:59 +0700

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Beranda from './screens/Beranda';
+
+import setelan from './screens/setelan';
+import notifikasi from './screens/notifikasi';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Beranda" component={Beranda} options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Notifikasi" component={notifikasi} options={{
+          tabBarLabel: 'Notifikasi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Setelan" component={setelan}options={{
+          tabBarLabel: 'Setelan',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+}
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/screens/notifikasi.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const notifikasi = () => {
+  return (
+    <View>
+      <Text>notifikasi</Text>
+    </View>
+  )
+}
+
+export default notifikasi
+
+const styles = StyleSheet.create({})
